e10b9eda593e8276236e2e8b09e1ea7bdeef1cce
Matt Fischer <matt@mattfischer.com>
2015-07-03 23:06:13 +0000

    Much cleaner with hiera

--- manifests/control.pp
+++ manifests/control.pp
@@ -1,20 +1,7 @@
 class deployments::control {
-    class { '::keystone':
-        admin_token     => 'super_secret',
-        package_ensure  => 'latest',
-        public_endpoint => 'http://localhost:5000',
-        admin_endpoint  => 'http://localhost:35357',
-    }
-    class { '::keystone::roles::admin':
-        email    => 'root@localhost',
-        password => 'admin_pass',
-    }
+    include ::keystone
+    include ::keystone::roles::admin
 
-    # XXX this needs to be in hiera, ensure all clients are the latest
-    # another keystone dep, need latest
-#    class { '::openstacklib::openstackclient':
-#        package_ensure => 'latest',
-#    }
     $keystone_deps = ['python-pbr', 'python-tz']
     package { $keystone_deps:
         ensure => 'latest',
