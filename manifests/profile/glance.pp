dbbe4d7bc3bcf6e99d49ad945caeff3090dedcea
Matt Fischer <matt@mattfischer.com>
2015-07-04 02:34:11 +0000

    Upload Cirros automatically

--- manifests/profile/glance.pp
+++ manifests/profile/glance.pp
@@ -1,4 +1,6 @@
-class deployments::profile::glance {
+class deployments::profile::glance(
+  $cirros_version = '0.3.4',    
+) {
   include ::glance
   include ::glance::api
   include ::glance::registry
@@ -7,4 +9,12 @@ class deployments::profile::glance {
 
   include ::mysql::server
   include ::glance::db::mysql
+
+  glance_image { 'cirros':
+    location         => "https://download.cirros-cloud.net/${cirros_version}/cirros-${cirros_version}-x86_64-disk.img",
+    name             => "Cirros ${cirros_version}",
+    is_public        => 'Yes', 
+    container_format => 'bare',
+    disk_format      => 'qcow2',
+  }
 }
