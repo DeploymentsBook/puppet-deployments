5740b92ad3928530f5bda34370146f92f9f5904b
Matt Fischer <matt@mattfischer.com>
2015-07-06 02:40:44 +0000

    Add rabbit users

--- manifests/profile/cinder.pp
+++ manifests/profile/cinder.pp
@@ -3,7 +3,7 @@ class deployments::profile::cinder
   include ::cinder
   include ::cinder::api
   include ::cinder::config
-  include ::cinder::volume
-  include ::cinder::keystone::auth
   include ::cinder::db::mysql
+  include ::cinder::keystone::auth
+  include ::cinder::volume
 }
--- manifests/profile/nova.pp
+++ manifests/profile/nova.pp
@@ -2,13 +2,14 @@ class deployments::profile::nova
 {
   include ::nova
   include ::nova::api
+  include ::nova::cert
+  include ::nova::db::mysql
+  include ::nova::conductor
+  include ::nova::consoleauth
+  include ::nova::keystone::auth
+  include ::nova::objectstore
+  include ::nova::rabbitmq
   include ::nova::scheduler
   include ::nova::scheduler::filter
-  include ::nova::objectstore
-  include ::nova::cert
-  include ::nova::consoleauth
-  include ::nova::conductor
   include ::nova::vncproxy
-  include ::nova::keystone::auth
-  include ::nova::db::mysql
 }
