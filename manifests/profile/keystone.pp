34e6dc612d2acc089a54ed97db9322fd89c61ed9
Emilien Macchi <emilien@redhat.com>
2015-08-24 16:40:07 -0400

    keystone: do not manage python dependencies
    Python dependencies are already managed by UCA packaging, so it's not
    useful to manually have this kind of Puppet code.

--- manifests/profile/keystone.pp
+++ manifests/profile/keystone.pp
@@ -3,10 +3,4 @@ class deployments::profile::keystone {
   include ::keystone::roles::admin
   include ::keystone::endpoint
   include ::keystone::db::mysql
-
-  $keystone_deps = ['python-pbr', 'python-tz']
-  package { $keystone_deps:
-    ensure => 'latest',
-    before => [Service['keystone'], Package['python-openstackclient']],
-  }
 }
