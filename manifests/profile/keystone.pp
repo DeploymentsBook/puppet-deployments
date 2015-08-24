b42604f361ce33fd0b447d695d6a8d013cdb1220
Emilien Macchi <emilien@redhat.com>
2015-08-24 16:41:38 -0400

    keystone: install in WSGI
    Eventlet is deprecated in Kilo and will be dropped in M cycle.
    We should use WSGI to make sure people understand the right way.

--- manifests/profile/keystone.pp
+++ manifests/profile/keystone.pp
@@ -1,6 +1,8 @@
 class deployments::profile::keystone {
+  include ::apache
   include ::keystone
   include ::keystone::roles::admin
   include ::keystone::endpoint
   include ::keystone::db::mysql
+  include ::keystone::wsgi::apache
 }
