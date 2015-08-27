6b150a50493d1ebc73db54410e743ce4d45c5dba
Matt Fischer <matt@mattfischer.com>
2015-08-27 04:34:38 +0000

    use openstack_extras to pull in UCA

--- manifests/profile/uca.pp
+++ manifests/profile/uca.pp
@@ -1,18 +1,12 @@
 # Ubuntu Cloud Archive
 class deployments::profile::uca (
-  $ensure = 'present',
-  $release = 'trusty-updates/kilo',
+  $release = 'kilo',
 ) {
   include ::apt
 
-  apt::source { 'ubuntu_cloud_archive':
-    ensure       => $ensure,
-    location     => "http://ubuntu-cloud.archive.canonical.com/ubuntu/${version}",
-    release      => $release,
-    include      => { 'src' => false },
-    architecture => $::architecture,
-    key          => '391A9AA2147192839E9DB0315EDB1B62EC4926EA',
-    notify       => Class['Apt::Update'],
+  class { '::openstack_extras::repo::debian::ubuntu':
+    release => $release,
+    package_require => true,
   }
 
   Apt::Source<||> -> Package <||>
