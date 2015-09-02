3e7938ca69ed4ea706e7130f9635dbecc9e2ddda
Matt Fischer <matt@mattfischer.com>
2015-09-02 04:01:30 +0000

    add metadata agent

--- manifests/profile/neutron.pp
+++ manifests/profile/neutron.pp
@@ -7,6 +7,7 @@ class deployments::profile::neutron
   include ::neutron::keystone::auth
   include ::neutron::plugins::ml2
   include ::neutron::agents::ml2::ovs
+  include ::neutron::agents::metadata
   include ::neutron::agents::l3
   include ::neutron::agents::dhcp
   include ::neutron::server::notifications
