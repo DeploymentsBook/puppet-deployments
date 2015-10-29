116512c3efd4022c02a47b201b2538bd9f1931cf
Matt Fischer <matt@mattfischer.com>
2015-10-29 07:08:47 +0000

    Add support for network/subnet creation

--- manifests/profile/neutron.pp
+++ manifests/profile/neutron.pp
@@ -11,4 +11,14 @@ class deployments::profile::neutron
   include ::neutron::agents::l3
   include ::neutron::agents::dhcp
   include ::neutron::server::notifications
+
+  $network_hash = hiera_hash('neutron_network', false)
+  if $network_hash {
+    create_resources('neutron_network', $network_hash)
+  }
+
+  $subnet_hash = hiera_hash('neutron_subnet', false)
+  if $subnet_hash {
+    create_resources('neutron_subnet', $subnet_hash)
+  }
 }
