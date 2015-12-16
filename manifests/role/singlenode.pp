7c14f58f1cded00360da2ff7274c4b0abf1846b5
Matt Fischer <matt@mattfischer.com>
2015-12-15 18:47:04 -0700

    Add swift role to single node

--- manifests/role/singlenode.pp
+++ manifests/role/singlenode.pp
@@ -1,6 +1,7 @@
 class deployments::role::singlenode {
   include deployments::role::compute
-  include deployments::role::control
+  include deployments::role::compute
+  include deployments::role::swift
 }
 
 include deployments::role::singlenode
