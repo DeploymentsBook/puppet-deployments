5fc3410d2637e982cd01932593be96d86b23b791
Matt Fischer <matt@mattfischer.com>
2015-12-27 07:35:28 -0700

    Add a hosts entry for the current machine

--- manifests/profile/base.pp
+++ manifests/profile/base.pp
@@ -2,4 +2,9 @@ class deployments::profile::base {
   include deployments::profile::openrcs
   include deployments::profile::uca
   include ::openstacklib::openstackclient
+
+  # add an alias to /etc/hosts to ensure sudo works
+  host { $hostname:
+    ip => $ipaddress,
+  }
 }
