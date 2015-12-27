7709c7338a8339d659cc0891b1516b0188c41a59
Matt Fischer <matt@mattfischer.com>
2015-12-27 18:12:01 +0000

    Add NTP support to base profile

--- manifests/profile/base.pp
+++ manifests/profile/base.pp
@@ -1,6 +1,7 @@
 class deployments::profile::base {
   include deployments::profile::openrcs
   include deployments::profile::uca
+  include ::ntp
   include ::openstacklib::openstackclient
 
   # add an alias to /etc/hosts to ensure sudo works
