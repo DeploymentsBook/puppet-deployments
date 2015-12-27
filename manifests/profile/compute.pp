d0dd6fb44fdd32bc665531d8b633f1e8596cc3c8
Matt Fischer <matt@mattfischer.com>
2015-12-27 08:36:00 -0700

    Add nova-neutron&ovs connectors for multinode
    This change adds the required neutron and ovs settings and packages
    to compute nodes. Compute nodes need OVS packages but also some neutron
    settings in nova.conf. ::neutron is included because it is needed
    to provide some references for neutron::agents::ml2::ovs. neutron
    is not actually running on the compute node however although some
    packages are installed.

--- manifests/profile/compute.pp
+++ manifests/profile/compute.pp
@@ -3,5 +3,8 @@ class deployments::profile::compute
   include ::nova
   include ::nova::compute
   include ::nova::compute::libvirt
+  include ::nova::compute::neutron
   include ::nova::network::neutron
+  include ::neutron
+  include ::neutron::agents::ml2::ovs
 }
