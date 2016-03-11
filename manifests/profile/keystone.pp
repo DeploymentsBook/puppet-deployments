cff97765a121ddbb2101fa34be34e1c0b1054a4e
Matt Fischer <matt@mattfischer.com>
2016-03-11 14:27:15 -0700

    Drop openstackclient dependency work-arounds

--- manifests/profile/keystone.pp
+++ manifests/profile/keystone.pp
@@ -7,12 +7,4 @@ class deployments::profile::keystone {
   include ::keystone::endpoint
   include ::keystone::db::mysql
   include ::keystone::wsgi::apache
-
-  # This is required due to packaging issues with python-openstackclient
-  # see LP: 1452025 et al
-  $keystone_deps = ['python-pbr', 'python-tz']
-  package { $keystone_deps:
-    ensure => 'latest',
-    before => [Service['keystone'], Package['python-openstackclient']],
-  }
 }
