1acbff960b36b74e7d4911811eae344bddfc93b2
Matt Fischer <matt@mattfischer.com>
2016-04-03 03:41:32 +0000

    Configure ceilometer polling for compute & cinder

--- manifests/profile/ceilometer.pp
+++ manifests/profile/ceilometer.pp
@@ -3,8 +3,10 @@ class deployments::profile::ceilometer
   include ::ceilometer
   include ::ceilometer::client
   include ::ceilometer::collector
+  include ::ceilometer::agent::auth
   include ::ceilometer::agent::central
   include ::ceilometer::agent::notification
+  include ::ceilometer::agent::polling
   include ::ceilometer::alarm::notifier
   include ::ceilometer::alarm::evaluator
   include ::ceilometer::api
