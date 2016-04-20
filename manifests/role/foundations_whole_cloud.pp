514f2a919fb7fe939583730d16ce4820c39e517a
Matt Fischer <matt@mattfischer.com>
2016-04-20 12:06:15 -0600

    Update and rename singlenode.pp to foundations_whole_cloud.pp

--- manifests/role/foundations_whole_cloud.pp
+++ manifests/role/foundations_whole_cloud.pp
@@ -1,8 +1,7 @@
-class deployments::role::singlenode {
+class deployments::role::foundations_whole_cloud {
   include deployments::role::foundations_block_storage
-  include deployments::role::foundations_compute
   include deployments::role::foundations_object_storage
   include deployments::role::foundations_public_cloud
 }
 
-include deployments::role::singlenode
+include deployments::role::foundations_whole_cloud
