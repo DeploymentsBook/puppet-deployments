c90d907bd5f9940c9aeeb921dd946a3e0a2a4f65
Matt Fischer <matt@mattfischer.com>
2016-04-20 11:52:21 -0600

    Update foundations_object_storage.pp
    Make this more like block storage

--- manifests/role/foundations_object_storage.pp
+++ manifests/role/foundations_object_storage.pp
@@ -1,8 +1,6 @@
 class deployments::role::foundations_object_storage {
-  include deployments::profile::base
-  include deployments::profile::keystone
+  include deployments::profile::foundations
   include deployments::profile::swift
-  include ::mysql::server
 }
 
 include deployments::role::foundations_object_storage
