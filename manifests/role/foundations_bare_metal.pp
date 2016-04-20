f4acffa667006970f87360b809ba43efa4bd62ef
Matt Fischer <matt@mattfischer.com>
2016-04-20 11:47:39 -0600

    Delete foundations_bare_metal.pp
    Unused & untested

--- manifests/role/foundations_bare_metal.pp
+++
@@ -1,6 +0,0 @@
-class deployments::role::foundations_bare_metal {
-  include deployments::role::foundations
-  include deployments::profile::ironic
-}
-
-include deployments::role::foundations_bare_metal
